da854c6b9540f1447125074c8c5bb9387a57d20e
Igor Silva <igor.silva@philips.com>
2023-08-24 14:06:37 -0300

    vercel configs

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,7 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+// export const api = "http://localhost:5000/api";
+// export const uploads = "http://localhost:5000/uploads";
+export const api = "https://reactgram-5kwqojcn3-iqsilva.vercel.app/api";
+export const uploads = "https://reactgram-5kwqojcn3-iqsilva.vercel.app/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
 
